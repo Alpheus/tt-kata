8bf77a488d3c9e6fe4c82e7a4e5b432f5142c81b
Alpheus <denis.cahuk@gmail.com>
2023-02-11 21:56:07 +0100

    Step 1, basic TDD tests

---
+++ world/deliveryEstimator.ts
@@ -0,0 +1,14 @@
+const PORT_DISTANCE = 1;
+const A_DISTANCE = 4;
+const B_DISTANCE = 5;
+
+class DeliveryEstimator {    
+    howLong(cargoManifest: string): number {
+        if (cargoManifest === '')
+            return 0;
+        
+        
+    }
+}
+
+export { DeliveryEstimator };
