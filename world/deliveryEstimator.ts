3e21738cd51c1cae3f9a76ca5abf755a20170915
Alpheus <denis.cahuk@gmail.com>
2023-02-12 01:08:06 +0100

    TDD Step 11 - FInal Refactor

--- world/deliveryEstimator.ts
+++ world/deliveryEstimator.ts
@@ -2,34 +2,43 @@ const TO_PORT = 1;
 const PORT_TO_A = 4;
 const B_DISTANCE = 5;
 
+type Destination = 'A' | 'B';
+type CargoManifest = Destination[];
+
+const TRUCK_RETURN_TIME: {[key in Destination]: number} = {
+    A: TO_PORT * 2,
+    B: B_DISTANCE * 2
+};
+
+const SHIP_RETURN_TIME = PORT_TO_A * 2;
+
 class DeliveryEstimator {
     howLong(cargoManifest: string): number {
-        const cargo = cargoManifest.split('');
+        const cargo = cargoManifest.split('') as CargoManifest;
 
-        let vehicleTime = { truckA: 0, truckB: 0, ship: 0 };
-        let availableTruck = 'truckA';
         let lastDelivery = 0;
+        let vehicleTime = { truckA: 0, truckB: 0, ship: 0 };
 
         for (let destination of cargo) {
-            let thisDelivery;
+            let availableTruck = vehicleTime.truckA > vehicleTime.truckB ? 'truckB' : 'truckA';
+            let thisDelivery: number = 0;
 
             switch (destination) {
                 case 'A':
                     let shipDeparture = Math.max(vehicleTime[availableTruck] + TO_PORT, vehicleTime.ship);
                     thisDelivery = shipDeparture + PORT_TO_A;
-                    vehicleTime.ship = shipDeparture + 2 * PORT_TO_A;
-                    vehicleTime[availableTruck] += 2 * TO_PORT;
+                    vehicleTime.ship = shipDeparture + SHIP_RETURN_TIME;
 
                     break;
                 case 'B':
                     thisDelivery = vehicleTime[availableTruck] + B_DISTANCE;
-                    vehicleTime[availableTruck] += 2 * B_DISTANCE;
 
                     break;
             }
 
+            vehicleTime[availableTruck] += TRUCK_RETURN_TIME[destination];
+
             lastDelivery = Math.max(lastDelivery, thisDelivery);
-            availableTruck = vehicleTime.truckA > vehicleTime.truckB ? 'truckB' : 'truckA';
         }
 
         return lastDelivery;
